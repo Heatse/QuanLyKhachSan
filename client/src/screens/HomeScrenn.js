9ef00896d21d7bee82af74fc16b6dc6595a57048
Heatse <117134442+Heatse@users.noreply.github.com>
2023-04-09 17:35:19 +0700

    Update HomeScrenn.js

--- client/src/screens/HomeScrenn.js
+++ client/src/screens/HomeScrenn.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Room from '../components/Room'
 
 function HomeScrenn() {
+    const [data, setData] = useState([])
+    const [rooms, setRooms] = useState([])
+    const [loading, setloading] = useState([])
+    const [error, seterror] = useState([])
+
+    useEffect(() => {
+        const gets = async () => {
+
+            setloading(true)
+            const response = (await axios.get('/api/rooms/getallrooms'))
+            setData(response.data)
+            setRooms(data)
+            setloading(false)
+            console.log(response.data)
+        }
+        gets();
+    }, [])
+
     return (
         <div>
-            <h1>HomeScrenn</h1>
+            <div className='row'>
+                {loading ? (<h1>Loading....</h1>) : (data.map(room => {
+
+                    return <div className='com-md-9'>
+                        <Room room={room} />
+                    </div>
+                }))}
+            </div>
         </div>
     )
 }
